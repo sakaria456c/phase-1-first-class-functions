d845b9853104ac70e71fe678904fd4d34da57f20
sakaria <sakariafarah@yahoo.com>
2022-05-09 17:56:15 -0500

    commit tree e03aae14250aa5e03429dca52e7ca84fed27018c

---
+++ index.js
@@ -0,0 +1,17 @@
+function receivesAFunction (callback) {
+    return (callback());
+}
+
+ function returnsANamedFunction (){
+    return function back(){
+
+    }
+ }
+
+ function returnsAnAnonymousFunction () {
+     return function(){
+         
+     }
+ }
+
+
